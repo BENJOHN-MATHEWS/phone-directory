ede9204b751806500d9dee814770ed302a1c7910
BENJOHN-MATHEWS <benjohn.mathews24j@gmail.com>
2021-05-27 13:06:31 +0530

    Header Component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <button>Add</button>
         <div>
           <span>Name</span><br />
